0ae90be60325ad05263fb0be492226ed5ef13fae
Nayan Jagetiya <nayan.jagetiya@brevitaz.com>
2025-05-28 07:38:56 +0530

    Mark the JPA package explicitly as it was giving warnings for redis

--- src/main/java/com/nayan/obai/inventory/InventoryServiceApplication.java
+++ src/main/java/com/nayan/obai/inventory/InventoryServiceApplication.java
@@ -2,7 +2,9 @@ package com.nayan.obai.inventory;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
+@EnableJpaRepositories(basePackages = "com.nayan.obai.inventory.repository")
 @SpringBootApplication
 public class InventoryServiceApplication {
 
